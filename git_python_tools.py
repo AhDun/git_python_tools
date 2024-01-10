5ebf1681c2321243f46ff4ab5d94a44aa11f1bde
AhDun <123>
2024-01-10 10:55:18 +0800

    增加开始与结束提示

--- git_python_tools.py
+++ git_python_tools.py
@@ -81,6 +81,7 @@ shell_win_scroll.config(command=shell_win.yview)
 
 shell_win.tag_config('T1C',foreground="#000000", font=("", 20, "bold"),justify="center")
 shell_win.tag_config('T2C',foreground="#000000", font=("", 15, "bold"),justify="center")
+shell_win.tag_config('T2CG',foreground="#007000", font=("", 15, "bold"),justify="center")
 shell_win.tag_config('T3C',foreground="#000000", font=("", 12, "bold"),justify="center")
 
 shell_win.tag_config('T1',foreground="#000000", font=("", 20, "bold"))
@@ -104,9 +105,11 @@ def shell_is_error():
     else:
         return 1
     
-# def shell_win_get_lines():
-#     index  = shell_win.index("end-1c")
-#     return index.split('.')[0]
+def shell_win_insert_separator(text):
+    text = '\n--------------------'+text+'-----------------------\n\n'
+    shell_win.insert(END, text,'T2CG')
+    shell_win.see(END)
+    shell_win.update()
     
 def shell(cmd):
     #res = subprocess.Popen(cmd.split(),stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
@@ -133,8 +136,9 @@ def shell_win_insert(cmd):
         shell_win.insert(END, datetime.datetime.now().strftime("[%H:%M:%S]") + shell_output + '\n','info')
     else:
         shell_win.insert(END, datetime.datetime.now().strftime("[%H:%M:%S]") + shell_error + '\n','error')
-    root.update()
     shell_win.see(END)
+    root.update()
+
 
 # def shell_win_insert(cmd):
 #     shell_win_insert(res)
@@ -147,11 +151,11 @@ def git_exist():
         return False
 
 def git_create():
-
+    shell_win_insert_separator('开始新建仓库')
     if touch_gitignore_file :
         file = open('.gitignore','a+')
 
-        file.write('git_python_tool.py\n')
+        file.write('\ngit_python_tool.py\n')
 
         file.close()
 
@@ -165,20 +169,27 @@ def git_create():
     if shell_is_error() == 0:
         if (shell_output.find("Reinitialized existing Git repository")) != -1:
             messagebox.showerror("错误", "这里已经有仓库了")
+            return
         else:
             messagebox.showinfo("成功", "仓库创建成功")
+            
     else:
         messagebox.showerror("错误", "发生异常")
+        return
+    shell_win_insert_separator('新建仓库完成')
 
 def git_remove():
+    shell_win_insert_separator('开始删除仓库')
     if git_exist():
         return
     if messagebox.askyesno("警告！", "确定要删除仓库吗？") == False:
         return
     res = shell_win_insert("rm -rf .git")
     messagebox.showinfo("成功", "仓库删除成功")
+    shell_win_insert_separator('删除仓库完成')
 
 def git_reset():
+    shell_win_insert_separator('开始重置仓库')
     if git_exist():
         return
     if messagebox.askyesno("警告！", "确定要重置仓库吗？") == False:
@@ -186,6 +197,7 @@ def git_reset():
     res = shell_win_insert("rm -rf .git")
     res = shell_win_insert("git init")
     messagebox.showinfo("成功", "仓库重置成功")
+    shell_win_insert_separator('重置仓库完成')
 
 def git_log():
     if git_exist():
@@ -267,6 +279,7 @@ def git_remote_url_check():
         
 
 def git_add_commit_push():
+    shell_win_insert_separator('开始本地提交并仓库推送')
     if git_exist():
         return
     res = shell_win_insert("git add .")
@@ -295,14 +308,17 @@ def git_add_commit_push():
     print(text)
     if text.find(default_branch + ' ->') != -1:
         messagebox.showinfo("成功", "推送成功")
+        shell_win_insert_separator('开始本地提交并仓库推送完成')
         return
     if text.find('Everything up-to-date') != -1:
         messagebox.showwarning("警告", "远程仓库已是最新的")
         return
     messagebox.showerror("错误", "推送失败")
+   
     
 
 def git_add_commit():
+    shell_win_insert_separator('开始仓库提交')
     if git_exist():
         return
     res = shell_win_insert("git add .")
@@ -316,12 +332,15 @@ def git_add_commit():
     res = shell_win_insert("git commit -m" + commit)
     if shell_is_error() == 1:
         messagebox.showerror("错误", "提交失败")
-
+        return
     else:
         messagebox.showinfo("成功", "提交成功")
+    shell_win_insert_separator('仓库提交完成')
 
     
 def git_push():
+    shell_win_insert_separator('开始仓库推送')
+
     if git_exist():
         return
     switch = simpledialog.askstring(title='',prompt='输入提交分支(不输入默认'+default_branch+')')
@@ -336,14 +355,16 @@ def git_push():
     print(text)
     if text.find(default_branch + ' ->') != -1:
         messagebox.showinfo("成功", "推送成功")
-        return
+        
     if text.find('Everything up-to-date') != -1:
         messagebox.showwarning("警告", "远程仓库已是最新的")
         return
     messagebox.showerror("错误", "推送失败")
-    return
+    
+    shell_win_insert_separator('仓库推送完成')
     
 def git_clone():
+    shell_win_insert_separator('开始克隆仓库')
     url = simpledialog.askstring(title='',prompt='输入克隆地址')
     if url  == None:
         return
@@ -351,11 +372,15 @@ def git_clone():
     text = shell_error
     if text.find('Cloning into') != -1:
         messagebox.showinfo("成功", "克隆成功")
+        
     else:
         messagebox.showerror("错误", "克隆异常")
+        return
+    shell_win_insert_separator('克隆仓库完成')
  
     
 def git_pull():
+    shell_win_insert_separator('开始拉取仓库')
     if git_exist():
         return
     res = shell_win_insert("git pull")
@@ -365,12 +390,12 @@ def git_pull():
     text = shell_output
     if text.find('Already up to date.') != -1:
         messagebox.showwarning("警告", "本地仓库已经是最新的")
-        return
-
+        
+    shell_win_insert_separator('拉取仓库完成')
     
 def git_back():
-
-    if messagebox.askyesno("警告！", "确定要回滚到上提交点吗？") == False:
+    shell_win_insert_separator('开始回滚到上次提交点')
+    if messagebox.askyesno("警告！", "确定要回滚到上次提交点吗？") == False:
         return
 
     git_remote_url_check()
@@ -380,9 +405,11 @@ def git_back():
         return
     else:
         messagebox.showinfo("成功", "回滚成功")
-        return
+        
+    shell_win_insert_separator('回滚到上次提交点完成')
     
 def git_back_commit():
+    shell_win_insert_separator('开始回滚到上次提交点')
     if messagebox.askyesno("警告！", "确定要回滚指定提交点吗？") == False:
         return
     commit = simpledialog.askstring(title='',prompt='输入提交的节点值')
@@ -395,7 +422,8 @@ def git_back_commit():
         return
     else:
         messagebox.showinfo("成功", "回滚成功")
-        return
+        
+    shell_win_insert_separator('回滚指定提交点完成')
 
 def start():
     shell_win.insert(END, logo,'logo')
